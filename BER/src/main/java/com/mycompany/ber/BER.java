c9fc80d8610aecb81047d2ec47e7745dc7083ed5
BERKABZ <150333064+BERKABZ@users.noreply.github.com>
2023-11-22 05:48:05 +0800

    Add files via upload

---
+++ BER/src/main/java/com/mycompany/ber/BER.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ber;
+
+/**
+ *
+ * @author USER
+ */
+public class BER {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
